caf988d9584a16e0e3d122d5207fcc09909fc889
SijbrenSchotanus <sts1990@hotmail.com>
2019-10-29 15:03:26 +0100

    first commit

---
+++ Maaltijden/src/main/java/com/maaltijd/Maaltijden/MaaltijdenApplication.java
@@ -0,0 +1,13 @@
+package com.maaltijd.Maaltijden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaltijdenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaltijdenApplication.class, args);
+	}
+
+}
---
+++ Maaltijden/src/test/java/com/maaltijd/Maaltijden/MaaltijdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaltijd.Maaltijden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaltijdenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
